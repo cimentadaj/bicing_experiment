2e19c4d952063561cbaff308306f735d8b09739a
cimentadaj <cimentadaj@gmail.com>
2017-12-01 13:16:14 +0100

    Automatic commit for bicing

--- daily_job.sh
+++ daily_job.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-ssh-add /Users/cimentadaj/.ssh/cimentadaj-GitHub.pub
-
 cd /Users/cimentadaj/Downloads/gitrepo/bicing_experiment
 
 /usr/local/bin/RScript /Users/cimentadaj/Downloads/gitrepo/bicing_experiment/scrape_bicing.R
