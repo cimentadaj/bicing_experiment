9dc5a7f67f2edf025f39b7769394137628e77d86
cimentadaj <cimentadaj@gmail.com>
2017-12-01 12:57:13 +0100

    Automatic commit for bicing

--- daily_job.sh
+++ daily_job.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
+ssh-add /Users/cimentadaj/.ssh/id_rsa
+
 cd /Users/cimentadaj/Downloads/gitrepo/bicing_experiment
 
 /usr/local/bin/RScript /Users/cimentadaj/Downloads/gitrepo/bicing_experiment/scrape_bicing.R
 
 git add .
 git commit -m "Automatic commit for bicing"
-
-git remote set-url origin https://github.com/cimentadaj/bicing_experiment.git
 
 git push origin master
