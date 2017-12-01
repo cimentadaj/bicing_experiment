d6105707ac16a6c5a9c2cbc43aee4af393a98e10
cimentadaj <cimentadaj@gmail.com>
2017-12-01 15:19:29 +0100

    Reset empty .rds for scraping to begin today

--- daily_job.sh
+++ daily_job.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
-ssh-add /Users/cimentadaj/.ssh/cimentadaj-GitHub.pub
-
 cd /Users/cimentadaj/Downloads/gitrepo/bicing_experiment
 
 /usr/local/bin/RScript /Users/cimentadaj/Downloads/gitrepo/bicing_experiment/scrape_bicing.R
 
 git add .
 git commit -m "Automatic commit for bicing"
-
-git push origin master
