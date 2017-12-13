2b938ad1d650031266ed882e19a66bde41b381eb
cimentadaj <cimentadaj@gmail.com>
2017-12-13 12:57:49 +0100

    Update query for new time

--- sql_query.sql
+++ sql_query.sql
@@ -1,2 +1,2 @@
 read PASS < pw.txt
-mysql -uroot -p$PASS bicing -e "SELECT id, error_msg, COUNT(*) AS count FROM bicing_station WHERE time >= CONCAT(CURDATE(),' ','11:30:00') AND time <= CONCAT(CURDATE(),' ','12:00:00') GROUP BY id, error_msg;"
+mysql -uroot -p$PASS bicing -e "SELECT id, error_msg, COUNT(*) AS count FROM bicing_station WHERE time >= CONCAT(CURDATE(),' ','13:00:00') AND time <= CONCAT(CURDATE(),' ','14:01:00') GROUP BY id, error_msg;"
