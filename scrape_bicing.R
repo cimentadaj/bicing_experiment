322a34bb25e07ca3849bd7e2d12acd13a5b3b149
cimentadaj <cimentadaj@gmail.com>
2017-12-13 13:18:48 +0100

    Delete comments + read pw from txt file, more secure

--- scrape_bicing.R
+++ scrape_bicing.R
@@ -3,33 +3,6 @@ library(jsonlite)
 library(DBI)
 library(tidyverse)
 
-# # grab the list of datasets from the API
-
-# open_data <-
-#   GET("http://opendata-ajuntament.barcelona.cat/data/api/3/action/package_list",
-#       config = list(`content-type` = "application/json"))
-# 
-# # Grab the names to identify the bicing data
-# data_names <-
-#   content(open_data)[-c(1, 2)] %>%
-#   .[[1]] %>%
-#   as.character()
-# 
-# 
-# bicycle_names <-
-#   data_names %>%
-#   map_lgl(~ grepl("bic", .x)) %>%
-#   data_names[.]
-
-# For how to package an api
-# http://conjugateprior.org/2017/06/packaging-the-twfy-api/
-
-# to give permission to run scrap
-# chmod +x daily_job.sh
-# chmod +x scrape_bicing.R
-
-# wd <- "/home/cimentadaj/bicycle"
-
 test_url <-
   paste0(
     "http://opendata-ajuntament.barcelona.cat/data/api/3/action/resource_search?query=name:",
@@ -55,7 +28,7 @@ safe_GET <- safely(my_GET)
 
 safe_request <- safe_GET(bicycle_url)
 
-# Bcn time (+1 because the server is in amsterdam)
+# Bcn time
 current_time <- as.character(lubridate::now())
 
 # If there's an error, return an empty df with the error in the error column
@@ -103,29 +76,20 @@ if (!is.null(safe_request$error)) {
   }
 }
 
+# Get password from pw.txt, also used from the scheduele MySQL query
+pw <- readLines("pw.txt")
+
+# Connect to the database
 con <- dbConnect(RMySQL::MySQL(),
                  dbname = "bicing",
                  user = "cimentadaj",
-                 password = "Lolasouno2",
+                 password = pw,
                  port = 3306)
 
-# dbListTables(con)
-
-# main_data <- as_tibble(dbReadTable(con, "bicing_station"))
-
-
-# binded_data <-
-#   bind_rows(main_data, summary_bicing)
-
+# Append the API request df to the bicing_station table from the database
 write_success <-
   dbWriteTable(conn = con, "bicing_station", summary_bicing,
                append = TRUE, row.names = FALSE)
 
-# # 6 hours a days gives 360 rows by 30 days
-# (60 * 6) * 30
-# 
-# # A df with 11k rows is only 0.4 MB, so you can store it on github
-# 
-# df_large <- map_dfr(1:131400, ~ iterative_bicing[1, ])
-
+# If write is a success, print success
 if (write_success) print("Append success") else print("No success")
