da8091ec4b06ffd1bd2941f1e2f1828c8600af63
cimentadaj <cimentadaj@gmail.com>
2017-12-13 11:29:45 +0100

    Create/ignore pw txt file + add secure pw to sql_query

--- sql_query.sql
+++ sql_query.sql
@@ -1,1 +1,2 @@
-mysql -uroot -pLolasouno2 bicing -e "SELECT id, error_msg, COUNT(*) AS count FROM bicing_station WHERE time >= CONCAT(CURDATE(),' ','11:30:00') AND time <= CONCAT(CURDATE(),' ','12:00:00') GROUP BY id, error_msg;"
+read PASS < pw.txt
+mysql -uroot -p$PASS bicing -e "SELECT id, error_msg, COUNT(*) AS count FROM bicing_station WHERE time >= CONCAT(CURDATE(),' ','11:30:00') AND time <= CONCAT(CURDATE(),' ','12:00:00') GROUP BY id, error_msg;"
