c375f0034f2fdb5e8d8120d6e4240d5d52828cf6
cimentadaj <cimentadaj@gmail.com>
2020-11-24 14:41:40 +0000

    Cleans up repo for redoing whole project

--- create_db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE bicing
-
-USE bicing;
-
--- Grant access to a username from any IP
--- The % means that it accepts any IP
-CREATE USER 'username'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON bicing.* TO 'username'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
